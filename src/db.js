24b54327c6be261b8d87286612548f61bc02d83d
Trehaan Parekh <trehaan2017@gmail.com>
2025-04-23 23:39:45 +0530

    Added dotenv

--- src/db.js
+++ src/db.js
@@ -1,11 +1,14 @@
 import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = mysql2.createPool(
     {
         host : process.env.DB_HOST,
         user : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
-        database : "pokemon_db"
+        database : process.env.DB_NAME
     }
 ).promise();
 
